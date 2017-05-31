b5d186b5970dabe92e456c5599e84cfec008aac4
manujosephv <manujosephv@gmail.com>
2017-05-31 21:00:07 +0530

    test

--- run_task.py
+++ run_task.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+import os
+import sys
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MovieScraper.settings")
 from movielistview.tasks import delete_read_movies_task
 
 print "in run task"
