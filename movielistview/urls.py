c4f31e33919789dd8b252d9b760959deb01d7bf9
manujosephv <manujosephv@gmail.com>
2017-06-02 20:17:14 +0530

    added proxy button

--- movielistview/urls.py
+++ movielistview/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^view_movies/$', views.view_movies, name='view_movies'),
     url(r'^filter_movies/$', views.filter_movies, name='filter_movies'),
     url(r'^mark_read_movies/$', views.mark_read_movies, name='mark_read_movies'),
-    url(r'^update_ratings/$', views.update_ratings, name='update_ratings'),
+    # url(r'^update_ratings/$', views.update_ratings, name='update_ratings'),
     url(r'^poll_state/$', views.poll_state, name='poll_state'),
 ]
 
