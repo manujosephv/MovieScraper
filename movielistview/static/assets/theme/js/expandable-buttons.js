a1e950ec6d1de619c7f09fab752b7d48601cfe47
manujosephv <manujosephv@gmail.com>
2017-06-11 07:50:17 +0530

    added form elements and hooked up animations.

--- movielistview/static/assets/theme/js/expandable-buttons.js
+++ movielistview/static/assets/theme/js/expandable-buttons.js
@@ -89,3 +89,23 @@ function toggleForm(id){
 //    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 //    return pattern.test(email);
 //};
+
+//Form inside
+
+        $('.search-wrapper').find('.fa-search').on('click', function(){
+            var id = $(this).parent('.search-wrapper').prop('id');
+            console.log(id + "clicked") ;
+            $('#'+id+'.search-wrapper').addClass('focused')
+            $('#'+id+'.search-wrapper').find('.fa-search').addClass('hide')
+            $('#'+id+'.search-wrapper').find('.fa-minus-circle').removeClass('hide')
+//            $('#'+id+'.search-wrapper').find('.fa-search').toggleClass('fa-search','fa-close');
+        });
+        
+        $('.search-wrapper').find('.fa-minus-circle').on('click',function(){
+            if (!$(this).hasClass('hide')){
+                    $(this).parent('.search-wrapper').removeClass('focused');
+                    $(this).addClass('hide');
+                    $(this).siblings('.fa-search').removeClass('hide')
+                    $(this).siblings('.search-input').val('');
+            }
+        });
